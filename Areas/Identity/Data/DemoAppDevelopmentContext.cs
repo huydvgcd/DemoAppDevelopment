bf4901bb9056fce477b6ca3a4609676a936119b2
Administrator <Administrator@21AK22-COM>
2022-08-23 23:15:20 +0700

    Config Login, Register. Add role

---
+++ Areas/Identity/Data/DemoAppDevelopmentContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DemoAppDevelopment.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DemoAppDevelopment.Data
+{
+    public class DemoAppDevelopmentContext : IdentityDbContext<DemoAppDevelopmentUser>
+    {
+        public DemoAppDevelopmentContext(DbContextOptions<DemoAppDevelopmentContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using DemoAppDevelopment.Areas.Identity.Data;
+using DemoAppDevelopment.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(DemoAppDevelopment.Areas.Identity.IdentityHostingStartup))]
+namespace DemoAppDevelopment.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<DemoAppDevelopmentContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("DemoAppDevelopmentContextConnection")));
+
+                services.AddDefaultIdentity<DemoAppDevelopmentUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<DemoAppDevelopmentContext>();
+            });
+        }
+    }
+}
