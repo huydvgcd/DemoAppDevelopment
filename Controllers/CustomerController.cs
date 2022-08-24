22da1fb3d37222b4cd029aa5cb01b7cf0e4ca581
Administrator <Administrator@21AK22-COM>
2022-08-24 23:55:34 +0700

    Add customer controller & Index View

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,29 @@
+﻿using DemoAppDevelopment.Data;
+using DemoAppDevelopment.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DemoAppDevelopment.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public ApplicationDbContext _context;
+        public UserManager<ApplicationUser> _userManager;
+
+        public CustomerController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            List<Book> listBook = _context.Books.Include(b => b.Category).ToList();
+
+            return View(listBook);
+        }
+    }
+}
