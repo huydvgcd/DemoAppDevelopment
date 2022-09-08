33e28adbbd2cd49ace219c9dd4856cf6d23a816b
anhhuy <huyntagcd18655@fpt.edu.vn>
2022-09-08 14:42:57 +0700

    Fix BookViewModel

--- ViewModels/BookViewModel.cs
+++ ViewModels/BookViewModel.cs
@@ -9,10 +9,23 @@ namespace DemoAppDevelopment.ViewModels
     public class BookViewModel
     {
         public Book book { set; get; }
+
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Author { get; set; }
+
+        public int Price { get; set; }
+        public int Quantity { set; get; }
+
+        public Category Category { get; set; }
+
         public List<Category> listCategory { set; get; }
 
         [Required(ErrorMessage = "Please choose profile image")]
         [Display(Name = "Profile Picture")]
         public IFormFile ProfileImage { get; set; }
+        public string ImageUrl { get; set; }
     }
 }
